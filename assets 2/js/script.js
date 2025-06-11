917f598cde342244e1f4d9a718561e585db48243
nicoladomingorizzo <nicoladomingorizzo@gmail.com>
2025-06-11 16:35:40 +0200

    Refactor HTML structure and enhance JavaScript functionality for team member cards

--- assets 2/js/script.js
+++ assets 2/js/script.js
@@ -1,9 +1,8 @@
 /*
 Dato un array di oggetti rappresentante un team di un’azienda, creare una pagina dedicata in cui mostrare una card per ciascun componente.
-(trovate l’array del team all’interno della cartella in allegato)
 Bonus
 Rendere l’esercizio responsive, mandando a capo le card
-Aggiungere un form di agginta membri che permetta di visualizzare il nuovo membro sulla pagina (usate una foto qualunque, anche vostra se volete sentirvi parte del team! :sorridere:)
+Aggiungere un form di agginta membri che permetta di visualizzare il nuovo membro sulla pagina (usate una foto qualunque, anche vostra se volete sentirvi parte del team!
 */
 
 const teamMembers = [
@@ -47,21 +46,46 @@ const teamMembers = [
 
 /*
 Dato un array di oggetti rappresentante un team di un’azienda, creare una pagina dedicata in cui mostrare una card per ciascun componente.
-(trovate l’array del team all’interno della cartella in allegato)
 */
+const mainEl = document.getElementById('main');
+const mainRowEl = document.getElementById('main');
 
-<div class="card mb-3 col-sm-12 col-md-6 col-lg-4">
+// function for create member card
+function generateMemberCard(member) {
+  const { name, role, img, email } = member;
+  // generate the member card
+  const memberCard = `
+<div class="card mt-3 col-sm-12 col-md-6 col-lg-4">
   <div class="row g-0">
     <div class="col-md-4">
-      <img class="img-fluid" src="..." class="img-fluid rounded-start" alt="...">
+      <img class="img-fluid" src="../${img}" class="img-fluid rounded-start" alt="...">
     </div>
-
     <div class="col-md-8">
       <div class="card-body">
-        <h5 class="card-name">name</h5>
-        <p class="card-role">role</p>
-        <p class="card-email"><small class="text-body-secondary"><a href="#"></a></small></p>
+        <h5 class="card-name">${name}</h5>
+        <p class="card-role">${role}</p>
+        <p class="card-email"><small class="text-body-secondary"><a href="#">${email}</a></small></p>
       </div>
     </div>
   </div>
 </div>
+`
+  return memberCard
+};
+
+//function for 
+function printTeamMembers(teamMembers, newNodeEl) {
+  newNodeEl.innerHTML = '';
+  // loop for in teamMembers
+  for (let i = 0; i < teamMembers.length; i++) {
+    const member = teamMembers[i];
+    // generate the member card
+    const memberCard = generateMemberCard(member);
+
+    newNodeEl.insertAdjacentHTML('beforeend', memberCard);
+
+  }
+
+};
+//call the function 
+printTeamMembers(teamMembers, mainRowEl);
